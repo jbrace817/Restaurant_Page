685cf142b0833867b36d3ea53605a30b14772fe0
jbrace817 <48393754+jbrace817@users.noreply.github.com>
2023-11-18 08:18:35 -0500

    working setup with webpack.

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: { index: "./src/index.js" },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: true,
+      chunks: ["index"],
+      filename: "index.html",
+    }),
+  ],
+};
